85a8debfe97f0ddc951830a10f5f018287f4add2
Shital Sapkota <shital.sapkota1@gmail.com>
2025-03-21 13:08:09 +0200

    add theme for light and dark mode

--- frontend/src/theme/ThemeProvider.tsx
+++ frontend/src/theme/ThemeProvider.tsx
@@ -1,13 +1,29 @@
+import { PaletteMode } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider as MUIThemeProvider } from "@mui/material/styles";
-import { ReactNode } from "react";
+import { ReactNode, useMemo, useState } from "react";
 import { theme } from "./theme";
 import { ThemeContext } from "./themeContext";
 
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  const [mode, setMode] = useState<PaletteMode>("light");
+
+  const colorMode = useMemo(
+    () => ({
+      mode,
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    [mode]
+  );
+
+  // Fixed variable name conflict
+  const muiTheme = useMemo(() => theme(mode), [mode]);
+
   return (
-    <ThemeContext.Provider value={undefined}>
-      <MUIThemeProvider theme={theme}>
+    <ThemeContext.Provider value={colorMode}>
+      <MUIThemeProvider theme={muiTheme}>
         <CssBaseline />
         {children}
       </MUIThemeProvider>
